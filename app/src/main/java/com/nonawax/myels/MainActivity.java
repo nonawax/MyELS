db9352db7e72f38fbaf2416170f94d34375a1c23
nonawax <nonawax@gmail.com>
2018-08-30 18:36:53 +0900

    20180830

--- app/src/main/java/com/nonawax/myels/MainActivity.java
+++ app/src/main/java/com/nonawax/myels/MainActivity.java
@@ -2,9 +2,11 @@ package com.nonawax.myels;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends Activity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends Activity {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
 //                .addTestDevice(AdRequest.)
                 .build();
+        Toast.makeText(getApplicationContext(), adRequest.toString(), Toast.LENGTH_LONG).show();
         mAdView.loadAd(adRequest);
 
     }
---
+++ app/src/main/java/com/nonawax/myels/MyEls.java
@@ -0,0 +1,14 @@
+package com.nonawax.myels;
+
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class MyEls extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(this, "ca-app-pub-9716462781973217~8018798230");
+    }
+}
