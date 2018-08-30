fdd50d06e8a8c691623fecd8fe1f87e6c78d1467
nonawax <nonawax@gmail.com>
2018-08-30 13:43:48 +0900

    Admob 적용

--- app/src/main/java/com/nonawax/myels/MainActivity.java
+++ app/src/main/java/com/nonawax/myels/MainActivity.java
@@ -3,11 +3,21 @@ package com.nonawax.myels;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends Activity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
+
 }
